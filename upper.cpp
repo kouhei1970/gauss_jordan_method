b2d97cacd6ad41c2ae324800363f995d78863463
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-06-11 23:09:06 +0900

    完成
    ピポッド選択なしバージョンとして完成

--- upper.cpp
+++ upper.cpp
@@ -2,18 +2,35 @@
 
 short matPrint(double* mat, short M, short N);
 
-int main(){
-  double m=3;
-  double n=3;
-  double A[9]={
+int main()
+{
+  short m=3;
+  short n=3;
+  double A[9]=
+  {
     1.0,2.0,3.0,
     4.0,5.0,6.0,
     6.0,8.0,9.0
   };
+  short i,j,k;
+  double num,den;
   matPrint(A, m, n);
 
-
-
+  for (i=0; i<m; i++)
+  {
+    for (j=i+1; j<m; j++)
+    {
+      num=A[j*n+i];
+      den=A[i*n+i];
+      for (k=i; k<n; k++)
+      {
+        //printf("j=%d,i=%d,%f/%f\n", j,i,num, den);
+        A[j*n+k] = A[j*n+k] -  A[i*n+k] * num/den;
+      }
+      //matPrint(A, m, n);
+    }
+  }
+  matPrint(A, m, n);
 
 }
 
@@ -25,5 +42,6 @@ short matPrint(double* mat, short M, short N){
     }
     printf("\n");
   }
+  printf("\n");
 }
 
